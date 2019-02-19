f781e2c10c0be0c0bcbd0172bce4a57a23114172
Nicholas Cheong <nchls.czx@gmail.com>
2019-02-19 15:37:29 -0500

    create project shell

--- api/Startup.cs
+++ api/Startup.cs
@@ -25,6 +25,7 @@ namespace Fisher.Bookstore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Models.BookstoreContext>(options => options.UseInMemoryDatabase("Books"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
